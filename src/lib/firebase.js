bf4b5b0e9af164fd9e75befa292d6b2426a9b1fb
Esther-Omono <enitanatakere@gmail.com>
2023-04-18 14:31:34 +0100

    added lib

---
+++ src/lib/firebase.js
@@ -0,0 +1,5 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// 48:57 / 11:59/58
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import FirebaseContext from "./context/firebase"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <FirebaseContext.Provider value = {{ firebase, FieldValue }}>
+            <App />
+        </FirebaseContext.Provider>
+    </React.StrictMode>,
 )
